b4026f5b8d2d7f12a73bab1913983b5131c67437
Marine Jolibert <marine.jolibert@edu.devinci.fr>
2024-12-03 22:06:40 +0100

    Ajout de l'affichage des rues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,15 @@ import Map from "@/components/map/map"
 export default async function Home() {
     let arrondissementData = await fetch('http://localhost:3000/api/arrondissement')
     let arrondissements = await arrondissementData.json()
+    let voieData = await fetch('http://localhost:3000/api/voie')
+    let voies = await voieData.json()
     return (
         <div>
             <div className="h-[2vh]">
                 <h1>Visualiser les données</h1>
             </div>
             <div>
-                <Map arrondissements={arrondissements.data} />
+                <Map arrondissements={arrondissements.data} voies={voies.data} />
             </div>
         </div>);
 }
--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 import { MapContainer, TileLayer, Polyline, Tooltip, LayersControl, LayerGroup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import { TArrondissement } from "@/modules/arrondissement/type"
+import { TVoie } from '@/modules/voie/type';
+import { LatLngExpression } from 'leaflet';
 
 
-export default function Map({ arrondissements }: { arrondissements: TArrondissement[] }) {
-
+export default function Map({ arrondissements, voies }: { arrondissements: TArrondissement[], voies: TVoie[] }) {
     return (
         <MapContainer center={[48.8589, 2.3470]} zoom={13} scrollWheelZoom={false} style={{ height: '98vh', width: '100wh' }}
         >
@@ -31,6 +32,16 @@ export default function Map({ arrondissements }: { arrondissements: TArrondissem
                 </LayersControl.Overlay>
                 <LayersControl.Overlay name="Rues">
                     <LayerGroup>
+                        {voies.map((voie: TVoie) => {
+                            const coordinates = voie.geometry.coordinates;
+                            if (coordinates && coordinates.length > 0) {
+                                const positions = coordinates.map((coord: LatLngExpression[] | LatLngExpression[]) => [coord[1], coord[0]]);
+                                return <Polyline key={voie._id} color='blue' opacity={0.2} fill fillColor='black' fillOpacity={0.1} positions={positions} >
+                                    <Tooltip sticky>{voie.properties.l_longmin}</Tooltip>
+                                </Polyline>
+                            }
+                            return null;
+                        })}
                     </LayerGroup>
                 </LayersControl.Overlay>
                 <LayersControl.Overlay name="Évènements">
